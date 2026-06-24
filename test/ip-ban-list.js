58a773da7fd61e04d4a7df41187ed1df9a815707
hdegroote <75906619+HDegroote@users.noreply.github.com>
2026-06-24 18:50:44 +0200

    Unflake IP ban list logs test

--- test/ip-ban-list.js
+++ test/ip-ban-list.js
@@ -5,32 +5,36 @@ const createTestnet = require('hyperdht/testnet')
 const IdEnc = require('hypercore-id-encoding')
 const { spawnBlindPeerBin, waitForOutput } = require('./helpers')
 
-test('logs multiple IP ban list public keys through the CLI', async (t) => {
-  const { bootstrap } = await createTestnet(3, t.teardown)
-  const storage = path.join(await t.tmp(), 'blind-peer')
+for (let i = 0; i < 100; i++) {
+  test('logs multiple IP ban list public keys through the CLI', async (t) => {
+    const { bootstrap } = await createTestnet(3, t.teardown)
+    const storage = path.join(await t.tmp(), 'blind-peer')
 
-  const firstKey = IdEnc.encode(crypto.keyPair().publicKey)
-  const secondKey = IdEnc.encode(crypto.keyPair().publicKey)
+    const firstKey = IdEnc.encode(crypto.keyPair().publicKey)
+    const secondKey = IdEnc.encode(crypto.keyPair().publicKey)
 
-  const proc = spawnBlindPeerBin(
-    t,
-    '--storage',
-    storage,
-    '--bootstrap',
-    String(bootstrap[0].port),
-    '--ip-ban-list-key',
-    firstKey,
-    '--ip-ban-list-key',
-    secondKey
-  )
+    const proc = spawnBlindPeerBin(
+      t,
+      '--storage',
+      storage,
+      '--bootstrap',
+      String(bootstrap[0].port),
+      '--ip-ban-list-key',
+      firstKey,
+      '--ip-ban-list-key',
+      secondKey
+    )
 
-  const line = await waitForOutput(proc, 'IP ban list public keys')
-  const log = JSON.parse(line)
+    const line = await waitForOutput(proc, 'IP ban list public keys')
+    const log = JSON.parse(line)
 
-  // we only test simple logging here, the main logic is already tested in blind-peer
-  t.is(
-    log.msg,
-    `IP ban list public keys:\n  -${firstKey}\n  -${secondKey}`,
-    'logs all submitted IP ban list public keys'
-  )
-})
+    // we only test simple logging here, the main logic is already tested in blind-peer
+    t.is(
+      log.msg,
+      `IP ban list public keys:\n  -${firstKey}\n  -${secondKey}`,
+      'logs all submitted IP ban list public keys'
+    )
+
+    proc.kill()
+  })
+}
