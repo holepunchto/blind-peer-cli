2a8963d4f541a6331a941c2a4ed5faf49012f34b
Thang Nguyen <1080481+vietthang@users.noreply.github.com>
2026-08-06 17:21:41 +0700

    add discovery key on notification-error (#32)
    * add discovery key on notification-error
    
    * add public key in log

--- bin.js
+++ bin.js
@@ -185,7 +185,12 @@ const cmd = command(
       logger.warn(`Error while flushing the db: ${e.stack}`)
     })
     blindPeer.on('notification-error', async (e, connection, request) => {
-      logger.warn(`Notification error: ${e.stack}`)
+      logger.warn(
+        `Notification error: discoveryKey=%s publicKey=%s %s`,
+        b4a.toString(request.destination.discoveryKey, 'hex'),
+        idEnc.encode(connection.remotePublicKey),
+        e.stack
+      )
       if (flags.debug) {
         try {
           logger.debug(
